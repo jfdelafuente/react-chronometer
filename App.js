353f27afc8fd13ade85e0d0bce94147d3b9a46bc
jfdelafuente <josefcodelafuente@gmail.com>
2019-07-01 12:11:50 +0200

    start button

--- App.js
+++ App.js
@@ -6,11 +6,42 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       minutes: 0,
-      seconds: 55,
+      seconds: 0,
       millis: 0,
       running: false
     };
+    this._handleStartClick = this._handleStartClick.bind(this);
   }
+
+  _handleStartClick(event) {
+    if (!this.state.running) {
+      this.interval = setInterval(() => {
+        this.tick();
+      }, 100);
+      this.setState({ running: true });
+    }
+  }
+  tick() {
+    let millis = this.state.millis + 1;
+    let seconds = this.state.seconds;
+    let minutes = this.state.minutes;
+
+    if (millis === 10) {
+      millis = 0;
+      seconds = seconds + 1;
+    }
+    if (seconds === 60) {
+      millis = 0;
+      seconds = 0;
+      minutes = minutes + 1;
+    }
+    this.setState({
+      millis: millis,
+      seconds: seconds,
+      minutes: minutes
+    });
+  }
+
   render() {
     return (
       <div className="app">
@@ -25,7 +56,7 @@ export default class App extends React.Component {
 
         <div className="actions">
           <button className="btn start "
-            onClick={() => console.log("start")}>Start</button>
+            onClick={this._handleStartClick}>Start</button>
 
           <button className="btn stop "
             onClick={() => console.log("stop")}>Stop</button>
